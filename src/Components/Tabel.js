36bf403f088e98d086de02094fa9580a2cfa6bd7
Angga Prasetya W <phoviography@gmail.com>
2020-09-09 08:56:27 +0800

    add tugas 12

--- src/Components/Tabel.js
+++ src/Components/Tabel.js
@@ -9,6 +9,7 @@ const Tabel = ({ dataBuah }) => {
                     <th>Nama</th>
                     <th>Harga</th>
                     <th>Berat</th>
+                    <th>Action</th>
                 </tr>
                 {dataBuah.map(el => {
                     return (
@@ -16,9 +17,14 @@ const Tabel = ({ dataBuah }) => {
                             <td>{el.nama}</td>
                             <td>{el.harga}</td>
                             <td>{el.berat / 1000} Kg</td>
+                            <td className="buttonAction">
+                                <button class="button button1">Edit</button>
+                                <button class="button button2">Delete</button>
+                            </td>
                         </tr>
                     )
                 })}
+
             </table>
         </div>
     )
---
+++ src/Tugas12/Tugas12.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './Tugas12.css';
+import Tabel from '../Components/Tabel';
+
+class Tugas12 extends React.Component {
+    render() {
+        let dataHargaBuah = [
+            { nama: "Semangka", harga: 10000, berat: 1000 },
+            { nama: "Anggur", harga: 40000, berat: 500 },
+            { nama: "Strawberry", harga: 30000, berat: 400 },
+            { nama: "Jeruk", harga: 30000, berat: 1000 },
+            { nama: "Mangga", harga: 30000, berat: 500 }
+        ]
+        return (
+            <div className="HargaBuah">
+                <header>
+                    <h1 style={{ textAlign: "center" }}>Tabel Harga Buah</h1>
+                </header>
+                <div className="TabelBuah">
+                    <Tabel dataBuah={dataHargaBuah}/>
+                </div>
+            </div >
+        );
+    }
+}
+
+export default Tugas12;
